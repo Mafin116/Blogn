9fdbd7b242493a7d97caf129715a223020f00c9a
Mafin116 <uv00@list.ru>
2017-10-12 17:33:08 +0300

    Modified templest to display posts from database.1.1

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from .models import Post
 
 def post_list(request):
 	post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'blog/post_list.html', {'psots':posts})
+	return render(request, 'blog/post_list.html', {'posts': posts})
